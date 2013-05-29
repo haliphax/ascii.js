91fd3e905f41b9448577d3985326fbef048020b6
Todd Boyd <haliphax@gmail.com>
2013-05-29 13:24:12 -0500

    added 80x50 support and split ascii into font-based dirs

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ MYPWD=${BASH_SOURCE[0]%/*}
 
 [ -f $MYPWD/ascii.list ] && rm $MYPWD/ascii.list
 
-for x in $MYPWD/ascii/*.{asc,txt} ; do
+for x in $MYPWD/ascii/*/* ; do
 	CURR=${x#*ascii/}
-	printf "\t\t\t<li><a href='#%s'>%s</a></li>\n" $CURR $CURR >> $MYPWD/ascii.list
+	NODIR=${CURR#*/}
+	printf "\t\t\t<li><a href='#%s'>%s</a></li>\n" $CURR $NODIR >> $MYPWD/ascii.list
 done
 
 /usr/bin/env php -r "echo str_replace('<!-- list -->', file_get_contents('${MYPWD}/ascii.list'), file_get_contents('${MYPWD}/index.tmpl'));" > $MYPWD/index.html

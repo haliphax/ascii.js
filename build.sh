0a2e6ca36af11a1ca9889a25b4b1114edbe91d1e
Todd Boyd <haliphax@gmail.com>
2014-07-25 23:43:31 -0500

    directory-insensitive sort

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ for x in $MYPWD/ascii/*/* ; do
 	printf "\t\t\t<li><a href='#%s'>%s</a></li>\n" $CURR $NODIR >> $MYPWD/ascii.list
 done
 
+cat $MYPWD/ascii.list | sort -t / -k 2,3 > $MYPWD/ascii.list.sorted
+mv $MYPWD/ascii.list.sorted $MYPWD/ascii.list
 cat $MYPWD/index.top.tmpl $MYPWD/ascii.list $MYPWD/index.bottom.tmpl > $MYPWD/index.html
-
 [ -f $MYPWD/ascii.list ] && rm $MYPWD/ascii.list

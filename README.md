b8490e871e4e44396c5631f3210dfcad4d719b5c
Todd Boyd <haliphax@gmail.com>
2014-07-31 18:37:46 -0500

    tidied up and commented

--- README.md
+++ README.md
@@ -25,13 +25,15 @@ Based on the parent folder of the ASCII file being rendered, ascii.js will decid
 
 * The *Topaz 500* font used for Amiga artwork comes from [TrueSchool](http://trueschool.se)
 * The *80x25 DOS* and *80x50 DOS* fonts used for CP437 artwork come from [ApolloSoft](http://www.apollosoft.de/ASCII/indexen.htm)
+  * *These DOS fonts have been altered; control character glyphs (1-31 and 127) are remapped for terminal-hostile ASCII artwork*
 
 ## ANSI support
 
 ANSI color/command codes are *tenuously* supported. Older ANSI works may be using cursor tricks that are difficult to replicate in a static fashion. If you have problems with a particular piece, try reloading it in a recent version of [PabloDraw](http://picoe.ca) and saving a new copy.
 
-* 8 background colors
-* 16 foreground colors
+* Basic [SGR code](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors) support
+  * 8 background colors
+  * 16 foreground colors
 * Minimal [CSI code](https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_codes) support (mainly CUD/CUF)
 * Doctored font files for displaying extended CP437 characters (ex. 1-31 and 127)
 
--- js/ascii.js
+++ js/ascii.js
@@ -6,7 +6,12 @@ var
 	, list
 ;
 
+/***
+ * when the hash in the URL changes, either load the main menu
+ * or display the specified artwork.
+ ***/
 hashchange = function() {
+	// no filename; load main menu
 	if(! window.location.hash.match(/#.+/)) {
 		document.body.setAttribute('class', 'topaz500');
 		document.body.innerHTML = list;
@@ -14,15 +19,17 @@ hashchange = function() {
 	}
 
 	var
-		ascii = window.location.hash.substring(1)
-		, asciiClass
-		, xhr = new XMLHttpRequest()
+		ascii = window.location.hash.substring(1) // filename
+		, asciiClass // which font?
+		, xhr = new XMLHttpRequest() // AJAX request for pulling file
 	;
 
+	// AJAX response handler
 	xhr.onreadystatechange = function() {
 		if(xhr.readyState != 4) return;
-		
+
 		var
+			// content of the file, slightly altered
 			content = xhr.responseText
 				.replace(/&/g, '&amp;') // htmldecode
 				.replace(/</g, '&lt;')
@@ -31,20 +38,25 @@ hashchange = function() {
 				.replace(/\x1b\[\d*D/g, '') // unsupported ANSI sequences
 				.replace(/\x1b\[A\n\x1b\[\d*C/g, '')
 				.replace(/\x1a(?:.|\n)*$/, '') // hide SAUCE
+			// parts of the filename
 			, parts = ascii.match(/(?:([^\/]+)\/)?(.+\.[a-z]+)$/i)
 		;
-		
+
+		// if you find \x1b, run it through the ANSI formatter
 		if(content.indexOf('\x1b') > -1) {
 			content = ansiFormat(content);
 		}
 
+		// convert unprintable ASCII characters to their adjusted glyphs
 		content = convertSpecialChars(content);
-		
+
+		// display the header nav and artwork
 		document.body.innerHTML =
 			'<a href="#">home</a> // <a title="raw" href="ascii/' + ascii + '">'
 			+ parts[2] + '</a>' + '\n\n' + content
 		;
-		
+
+		// which font to use?
 		switch(parts[1]) {
 			case 'dos':
 				asciiClass = 'dos437';
@@ -57,140 +69,172 @@ hashchange = function() {
 				asciiClass = 'topaz500';
 				break;
 		}
-		
+
 		document.body.setAttribute('class', asciiClass + ' ascii');
 	};
 
+	// pull the file
 	xhr.open('GET', 'ascii/' + ascii, true);
-	
+
+	// specify response charset if browser allows it
 	if(xhr.overrideMimeType) {
 		xhr.overrideMimeType('text/plain; charset=ISO-8859-1');
 	}
-	
+
 	xhr.setRequestHeader('Content-Type', 'text/plain; charset=ISO-8859-1');
 	xhr.send(null);
 };
 
+// bind the hash change event
 window.onhashchange = hashchange;
 
+/***
+ * when the page loads, grab its initial contents and use them for the
+ * 'main menu'. throw a warning if being viewed in Internet Explorer.
+ ***/
 window.onload = function() {
 	list = document.body.innerHTML;
-	
+
 	if(window.navigator.userAgent.match(/MSIE/)) {
 		alert('This will definitely look like crap in Internet Explorer.');
 	}
-	
+
 	if(window.location.href.match(/#/)) {
 		hashchange();
 	}
 };
 
+/***
+ * replace ANSI SGR and CSI codes with their HTML/CSS equivalents.
+ * not all codes are supported; only 8 background and 16 foreground
+ * colors in the pallete.
+ ***/
 ansiFormat = function(content) {
 	var
-		bg = 0
-		, bold = false
-		, code = false
-		, column = 0
-		, copy = content
-		, counter
-		, div = document.createElement('div')
-		, fg = 7
-		, lastColumn = 0
-		, match
+		bg = 0 // background color
+		, fg = 7 // foreground color
+		, bold = false // bold flag
+		, started = false // have we started parsing?
+		, column = 0 // which column are we at?
+		, copy = content // temporary buffer of ASCII content
+		, match // holds regex matches
 		, rgx = {
-			ansi: /^\x1b\[[^Cm]*[Cm]/
-			, cursorForward: /\x1b\[(\d*)C/
-			, sgr: /\x1b\[((?:\d+;?)*)m/
+			ansi: /^\x1b\[[^Cm]*[Cm]/ // for stripping all CSI/SGR codes
+			, cursorForward: /\x1b\[(\d*)C/ // CUF
+			, sgr: /\x1b\[((?:\d+;?)*)m/ // SGR recognition
 		}
-		, snippet
-		, span
-		, transformed = ''
+		, div = document.createElement('div') // holds elements off-screen
+		, snippet // temporary string for building output
+		, span // HTML element with current SGR attributes
+		, transformed = '' // transformed ASCII
 	;
-	
+
+	// loop through ASCII one character at a time and parse SGR/CSI codes
 	for(var i = 0; i < content.length; i++) {
+		// any SGR/CSI codes?
 		try {
 			snippet = copy.match(rgx.ansi)[0];
 		} catch(ex) {
 			snippet = false;
 		}
-		
+
+		// yep; parse 'em
 		if(snippet) {
-			if(code == true) {
+			// start appending output after first pass
+			if(started == true) {
 				span.innerHTML = transformed;
 				div.appendChild(span);
 			}
-			
-			code = true;
+
+			started = true;
 			span = document.createElement('span');
 			transformed = '';
-			
+
+			// SGR parsing
 			if((match = snippet.match(rgx.sgr))) {
 				var params = match[1].split(';');
-				
+
 				for(var j = 0; j < params.length; j++) {
 					switch(true) {
+						// default
 						case (params[j] == 0):
 							fg = 7;
 							bg = 0;
 							bold = false;
 							break;
+						// bold
 						case (params[j] == 1):
 							bold = true;
 							break;
+						// normal
 						case (params[j] == 22):
 							bold = false;
 							break;
+						// foreground color
 						case (params[j] >= 30 && params[j] <= 37):
 							fg = params[j] - 30;
 							break;
+						// normal (fg only)
 						case (params[j] == 39):
 							fg = 7;
 							break;
+						// background color
 						case (params[j] >= 40 && params[j] <= 47):
 							bg = params[j] - 40;
 							break;
+						// normal (bg only)
 						case (params[j] == 49):
 							bg = 0;
 							break;
+						// unrecognized
 						default:
 							break;
 					}
 				}
-				
+
+				// "bold" changes color in our case, not font weight
 				if(bold == true && fg < 8) {
 					fg += 8;
 				}
+			// CUF parsing
 			} else if((match = snippet.match(rgx.cursorForward))) {
+				// @TODO attributes in empty space or not?
+
+				// how many spaces to move?
 				var count = (match[1] ? match[1] : 1);
-				
-				if(lastColumn != 0) {
-					count = lastColumn - count + 1;
-					lastColumn = 0;
-				}
-				
+
+				// do it
 				for(var j = 0; j < count; j++) {
 					column++;
-					
+
+					// wrap
 					if(column > 79) {
 						transformed += '\n';
 						column = 0;
 					}
-					
+
 					transformed += ' ';
 				}
 			}
-			
+
+			// apply attributes
 			span.setAttribute('class', 'bg' + bg + ' fg' + fg);
+			// strip CSI/SGR
 			copy = copy.replace(rgx.ansi, '');
+			// continue
 			i = content.length - copy.length - 1;
-		} else {			
+		// nothing to parse; just append output
+		} else {
+			// newline resets the column counter
 			if(content[i] != '\n') {
 				column++;
 			} else {
 				column = 0;
 			}
-			
+
+			// append output
 			transformed += content[i];
+			// remove from buffer
 			copy = copy.substring(1);
 		}
 	}
@@ -200,6 +244,10 @@ ansiFormat = function(content) {
 	return div.innerHTML;
 }
 
+/***
+ * convert certain unprintable ASCII characters to updated glyphs.
+ * takes advantage of the doctored DOS font files.
+ ***/
 convertSpecialChars = function(content)
 {
 	var
@@ -248,3 +296,4 @@ convertSpecialChars = function(content)
 }
 
 }());
+
